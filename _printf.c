eba7e32da3f81539fd82f14aed1994abf56f800e
timidgeek <5568@holbertonstudents.com>
2022-11-04 10:06:03 -0700

    moving struct above _printf function

--- _printf.c
+++ _printf.c
@@ -1,5 +1,27 @@
 #include "main.h"
 /**
+ * map_func - maps format specifiers to functions
+ * @ap: va_list that contains args
+ * @c: char (format i + 1)
+ * Return: 2 (number of characters printed)
+ */
+int map_func(va_list ap, char c)
+{
+	int j;
+	format_t f[] = {
+		{"c", print_char},
+		{"s", print_str},
+		{"d", print_int},
+		{"i", print_int}
+	};
+	for (j = 0; j < 4; j++)
+		if (*f[j].let == c)
+			return (f[j].func(ap));
+	_putchar('%');
+	_putchar(c);
+	return (2);
+}
+/**
  * _printf - produces output according to the format
  * @format: string that holds what will be printed
  * Return: NULL
@@ -33,26 +55,4 @@ int _printf(const char *format, ...)
 	}
 	va_end(ap);
 	return (charCount);
-}
-/**
- * map_func - maps format specifiers to functions
- * @ap: va_list that contains args
- * @c: char (format i + 1)
- * Return: 2 (number of characters printed)
- */
-int map_func(va_list ap, char c)
-{
-	int j;
-	format_t f[] = {
-		{"c", print_char},
-		{"s", print_str},
-		{"d", print_int},
-		{"i", print_int}
-	};
-	for (j = 0; j < 4; j++)
-		if (*f[j].let == c)
-			return (f[j].func(ap));
-	_putchar('%');
-	_putchar(c);
-	return (2);
 }
